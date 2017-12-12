2c598c2380f71d256e91a95a0d92e354030db92a
Mikita Saladkevich <Mikita_Saladkevich@epam.com>
2017-12-12 15:56:04 +0300

    Up to date.

---
+++ README.md
@@ -0,0 +1,11 @@
+#HOW TO USE
+
+GET http://localhost:8080/service/news?sources=bbc-news,abc-news&page=2&page_size=3
+
+	- fetch news from some source
+
+GET http://localhost:8080/service/sources?page=2&page_size=5
+
+	- fetch all news sources
+	
+	
--- src/main/java/com/news/core/SourceService.java
+++ src/main/java/com/news/core/SourceService.java
@@ -3,7 +3,6 @@ package com.news.core;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.news.beans.Source;
-import com.news.rest.client.RSClient;
 import com.news.util.Utility;
 import java.io.IOException;
 import org.json.simple.JSONObject;
@@ -12,23 +11,23 @@ import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Fetcher extension for pulling sources
- * 
- * @author Mikita_Saladkevich
- *
+ * For pulling sources
  */
-public class SourcesFetcher extends Fetcher {
+public class SourceService implements ServiceURL {
+
+    private Logger logger = Logger.getLogger(SourceService.class.getName());
 
     /**
      * Fetch all items of source. Do pagination by @page & @pageSize
      */
-    public List<Source> getSources(Map<String, String> params, Integer pageSize, Integer page) {
+    public List<Source> getSources(Integer pageSize, Integer page) {
 
         List<Source> roll = new ArrayList<>();
-        String plainJson = RSClient.getResponse(populateURL(params));
+        String plainJson = Utility.getResponse(getServiceURL());
 
         try {
 
@@ -37,22 +36,20 @@ public class SourcesFetcher extends Fetcher {
             roll = new ObjectMapper().readValue(articles, new TypeReference<List<Source>>() {});
 
         } catch (ParseException | IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "SourceService.getSources(). JSON parse.", e);
         }
 
-        return roll;
+        return Utility.getPage(roll, page, pageSize);
     }
 
     /**
-     * Create API request URL from .properties file & query params
+     * Create API request URL from .properties file
      * 
      * @return String API_URL
      */
-    public String getRequestURL() {
+    public String getServiceURL() {
         return Utility.getProperty("sources_url") + Utility.getProperty("apikey_prefix")
                 + Utility.getProperty("apikey_token");
     }
-
-    
 
 }
