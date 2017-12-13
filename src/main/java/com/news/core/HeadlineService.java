5c4b6f131e0707420b7b5ca19b0ee6cf934f20a6
Mikita Saladkevich <Mikita_Saladkevich@epam.com>
2017-12-13 17:47:09 +0300

    up

--- src/main/java/com/news/core/HeadlineService.java
+++ src/main/java/com/news/core/HeadlineService.java
@@ -11,7 +11,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
